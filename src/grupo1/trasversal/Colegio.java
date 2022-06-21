efca0386773295da99e33c26618c54fdbafa2cce
matydiaz434233 <102767133+matydiaz434233@users.noreply.github.com>
2022-06-21 17:45:14 -0300

    agrego clases data

---
+++ src/grupo1/trasversal/Colegio.java
@@ -0,0 +1,60 @@
+
+package grupo1.trasversal;
+
+/**
+ *
+ * @author Grupo 1
+ */
+import data.AlumnoData;
+import entidades.Materia;
+import data.Conexion;
+import entidades.Alumno;
+import java.sql.*;
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import javax.swing.JOptionPane;
+
+
+public class Colegio {
+
+    public static void main(String[] args) {
+        
+        Conexion conexion=new Conexion();
+        AlumnoData ad=new AlumnoData(conexion);
+        /*Alumno pepe=new Alumno("Leticia","Moreira",LocalDate.of(1973, Month.MARCH, 12),2344553,true);
+        
+        if(ad.agregarAlumno(pepe)){
+        
+            JOptionPane.showMessageDialog(null, "Alumno Agregado Exitosamente");
+        }
+*/
+        
+        List<Alumno> lista=ad.obtenerAlumnos();
+        
+        for(Alumno alu:lista){
+        
+            System.out.println("dni "+alu.getDni());
+            System.out.println("apellido "+alu.getApellido());
+            System.out.println("nombre "+alu.getNombre());
+        }
+        System.out.println("-------------Buscamos por id----------------");
+        Alumno aEncontrado=ad.obtenerAlumnoXId(3);
+        if(aEncontrado!=null){
+        System.out.println("Apellido "+aEncontrado.getApellido());
+        }else {
+        
+            System.out.println("Alumno no existe");
+        }
+        
+        System.out.println("-------------Modificamos apellido--------------");
+        //aEncontrado.setApellido("Zarate");
+        
+        //ad.modificarAlumno(aEncontrado);
+        System.out.println("-------------Borrar alumno--------------");
+        //ad.borrarAlumno(3);
+        
+    }
+}
---
+++ src/grupo1/trasversal/Data/MateriaData.java
@@ -0,0 +1,10 @@
+    
+package grupo1.trasversal.Data;
+
+/**
+ *
+ * @author Grupo 1
+ */
+public class MateriaData {
+    
+}
