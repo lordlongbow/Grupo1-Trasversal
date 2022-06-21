545e57f1189cb5f932924249be827fc18e1dac11
matydiaz434233 <102767133+matydiaz434233@users.noreply.github.com>
2022-06-21 20:21:40 -0300

    materia data

--- src/grupo1/trasversal/Colegio.java
+++ src/grupo1/trasversal/Colegio.java
@@ -1,13 +1,14 @@
-
 package grupo1.trasversal;
 
 /**
  *
  * @author Grupo 1
  */
-
 import grupo1.trasversal.Data.AlumnoData;
+import grupo1.trasversal.Modelos.Materia;
 import grupo1.trasversal.Data.Conexion;
+import grupo1.trasversal.Data.MateriaData;
+import grupo1.trasversal.Modelos.Alumno;
 import java.sql.*;
 import java.time.LocalDate;
 import java.time.Month;
@@ -15,8 +16,6 @@ import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import javax.swing.JOptionPane;
-import grupo1.trasversal.Modelos.Alumno;
-
 
 
 public class Colegio {
@@ -24,30 +23,30 @@ public class Colegio {
     public static void main(String[] args) {
         
         Conexion conexion=new Conexion();
-        AlumnoData ad=new AlumnoData(conexion);
-        /*Alumno pepe=new Alumno("Leticia","Moreira",LocalDate.of(1973, Month.MARCH, 12),2344553,true);
+        MateriaData md=new MateriaData(conexion);
+        Materia l=new Materia("Lengua",1,true);
         
-        if(ad.agregarAlumno(pepe)){
+        if(md.agregarMateria(l)){
         
-            JOptionPane.showMessageDialog(null, "Alumno Agregado Exitosamente");
+            JOptionPane.showMessageDialog(null, "Materia Agregada Exitosamente");
         }
-*/
+
         
-        List<Alumno> lista=ad.obtenerAlumnos();
+        List<Materia> lista=md.obtenerMaterias();
         
-        for(Alumno alu:lista){
+        for(Materia alu:lista){
         
-            System.out.println("dni "+alu.getDni());
-            System.out.println("apellido "+alu.getApellido());
+            System.out.println("año "+alu.getAnio());
+            System.out.println("activo "+alu.isActivo());
             System.out.println("nombre "+alu.getNombre());
         }
         System.out.println("-------------Buscamos por id----------------");
-        Alumno aEncontrado=ad.obtenerAlumnoXId(3);
-        if(aEncontrado!=null){
-        System.out.println("Apellido "+aEncontrado.getApellido());
+        Materia mEncontrado=md.obtenerMateriaXId(1);
+        if(mEncontrado!=null){
+        System.out.println("Nombre "+mEncontrado.getNombre());
         }else {
         
-            System.out.println("Alumno no existe");
+            System.out.println("Materia no existe");
         }
         
         System.out.println("-------------Modificamos apellido--------------");
--- src/grupo1/trasversal/Data/MateriaData.java
+++ src/grupo1/trasversal/Data/MateriaData.java
@@ -1,58 +1,47 @@
 package grupo1.trasversal.Data;
 
-/**
- *
- * @author Grupo 1
- */
+import grupo1.trasversal.Data.Conexion;
+import grupo1.trasversal.Modelos.Materia;
 import java.sql.Connection;
-import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import javax.swing.JOptionPane;
-import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import grupo1.trasversal.Modelos.Alumno;
-import grupo1.trasversal.Modelos.Materia;
+import javax.swing.JOptionPane;
 
+/**
+ *
+ * @author Grupo 1
+ */
 public class MateriaData {
-
     private Connection con = null;
-    private int idMateria;
-    private String nombre;
-    private int anio;
-    private boolean activo;
 
     public MateriaData(Conexion conexion) {
-
-        con = conexion.getConexion();
-
+      
+            con = conexion.getConexion();
+        
     }
-
+    
     public boolean agregarMateria(Materia materia) {
 
         boolean insert = true;
-        String sql = "INSERT INTO alumno (nombre, apellido, fechNac, dni, activo)  VALUES (?, ?, ?, ?, ?)";
+        String sql = "INSERT INTO materia (nombre, anio, activo)  VALUES (?, ?, ?)";
         try {
             PreparedStatement ps = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
-
-            ps.setString(1, alumno.getNombre());
-            ps.setString(2, alumno.getApellido());
-            ps.setDate(3, Date.valueOf(alumno.getFechNac()));//localDate a Date
-            ps.setLong(4, alumno.getDni());
-            ps.setBoolean(5, alumno.isActivo());
-
+            
+            ps.setString(1, materia.getNombre());
+            ps.setInt(2, materia.getAnio());
+            ps.setBoolean(3, materia.isActivo());
+            
             ps.executeUpdate();
-
+            
             ResultSet rs = ps.getGeneratedKeys();
 
             //JOptionPane.showMessageDialog(null, " Se agregó al alumno " + alumno + " correctamente");
             if (rs.next()) {
-                alumno.setIdAlumno(rs.getInt(1));
+                materia.setIdMateria(rs.getInt(1));
             } else {
                 // JOptionPane.showMessageDialog(null, "Error al intentar agregar al alumno");
                 insert = false;
@@ -60,146 +49,146 @@ public class MateriaData {
 
             ps.close();
         } catch (SQLException ex) {
-            insert = false;
-            if (ex instanceof java.sql.SQLIntegrityConstraintViolationException) {
-                JOptionPane.showMessageDialog(null, "Ya existe un alumno con ese dni ");
-            } else {
-
-                JOptionPane.showMessageDialog(null, "Error de sintaxis " + ex);
-
+            insert=false;
+            if(ex instanceof java.sql.SQLIntegrityConstraintViolationException){
+                JOptionPane.showMessageDialog(null, "Ya esta cargada la materia" );
+            }else {
+                JOptionPane.showMessageDialog(null, "Error de sintaxis "+ex );
             }
+            
 
         }
         return insert;
 
     }
-
-    public List<Alumno> obtenerAlumnos() {
-        ArrayList<Alumno> alumnos = new ArrayList<Alumno>();
+        
+        public List<Materia> obtenerMaterias() {
+        ArrayList<Materia> materias = new ArrayList<Materia>();
 
         try {
-            String sql = "SELECT * FROM alumno WHERE activo = 1;";
+            String sql = "SELECT * FROM materia WHERE activo = 1;";
             PreparedStatement ps = con.prepareStatement(sql);
-
+            
             ResultSet resultSet = ps.executeQuery();
-            Alumno alumno;
+            Materia materia;
             while (resultSet.next()) {
-                alumno = new Alumno();
-                alumno.setIdAlumno(resultSet.getInt("idAlumno"));
-                alumno.setApellido(resultSet.getString("apellido"));
-                alumno.setNombre(resultSet.getString("nombre"));
-                alumno.setFechNac(resultSet.getDate("fechNac").toLocalDate());
-                alumno.setDni(resultSet.getLong("dni"));
-                alumno.setActivo(resultSet.getBoolean("activo"));
+                materia = new Materia();
+                materia.setIdMateria(resultSet.getInt("idMateria"));
+                materia.setNombre(resultSet.getString("nombre"));
+                materia.setAnio(resultSet.getInt("anio"));
+                materia.setActivo(resultSet.getBoolean("activo"));
 
-                alumnos.add(alumno);
+                materias.add(materia);
             }
             ps.close();
         } catch (SQLException ex) {
-            JOptionPane.showMessageDialog(null, "Error al obtener alumnos");
+            JOptionPane.showMessageDialog(null,"Error al obtener materias");
         }
 
-        return alumnos;
+        return materias;
     }
-
-    public Alumno obtenerAlumnoXId(int id) {
-
-        Alumno alumno = null;
+        
+        public Materia obtenerMateriaXId(int id){
+     
+        Materia materia=null;
 
         try {
-            String sql = "SELECT * FROM alumno WHERE idAlumno = ? AND activo = 1;";
+            String sql = "SELECT * FROM materia WHERE idMateria = ? AND activo = 1;";
             PreparedStatement ps = con.prepareStatement(sql);
             ps.setInt(1, id);
             ResultSet resultSet = ps.executeQuery();
-
+            
             while (resultSet.next()) {
-                alumno = new Alumno();
-                alumno.setIdAlumno(resultSet.getInt("idAlumno"));
-                alumno.setApellido(resultSet.getString("apellido"));
-                alumno.setNombre(resultSet.getString("nombre"));
-                alumno.setFechNac(resultSet.getDate("fechNac").toLocalDate());
-                alumno.setDni(resultSet.getLong("dni"));
-                alumno.setActivo(resultSet.getBoolean("activo"));
+                materia = new Materia();
+                materia.setIdMateria(resultSet.getInt("idAlumno"));
+                materia.setNombre(resultSet.getString("nombre"));
+                materia.setAnio(resultSet.getInt("anio"));
+                materia.setActivo(resultSet.getBoolean("activo"));
 
             }
             ps.close();
         } catch (SQLException ex) {
-            JOptionPane.showMessageDialog(null, "Error al obtener alumnos");
+            JOptionPane.showMessageDialog(null,"Error al obtener la materia");
         }
 
-        return alumno;
-    }
-
-    public Alumno obtenerAlumnoXDNI(long dni) {
-
-        Alumno alumno = null;
+        return materia;
+     }
+        
+        
+        public Materia obtenerMateriaXAnio(int anio){
+     
+        Materia materia=null;
 
         try {
-            String sql = "SELECT * FROM alumno WHERE dni = ? AND activo = 1;";
+            String sql = "SELECT * FROM materia WHERE anio = ? AND activo = 1;";
             PreparedStatement ps = con.prepareStatement(sql);
-            ps.setLong(1, dni);
+            ps.setInt(1, anio);
             ResultSet resultSet = ps.executeQuery();
-
+            
             while (resultSet.next()) {
-                alumno = new Alumno();
-                alumno.setIdAlumno(resultSet.getInt("idAlumno"));
-                alumno.setApellido(resultSet.getString("apellido"));
-                alumno.setNombre(resultSet.getString("nombre"));
-                alumno.setFechNac(resultSet.getDate("fechNac").toLocalDate());
-                alumno.setDni(resultSet.getLong("dni"));
-                alumno.setActivo(resultSet.getBoolean("activo"));
+                materia = new Materia();
+                materia.setIdMateria(resultSet.getInt("idAlumno"));
+                materia.setNombre(resultSet.getString("nombre"));
+                materia.setAnio(resultSet.getInt("anio"));
+                materia.setActivo(resultSet.getBoolean("activo"));
 
             }
             ps.close();
         } catch (SQLException ex) {
-            JOptionPane.showMessageDialog(null, "Error al obtener alumnos");
+            JOptionPane.showMessageDialog(null,"Error al obtener la materia");
         }
 
-        return alumno;
+        return materia;
+     }
+        
+        
+        public boolean borrarMateria(int id){
+     
+         boolean borrado=false;
+         String sql= "UPDATE materia SET activo = 0 WHERE idMateria = ?";
+         try {
+             
+             PreparedStatement ps= con.prepareStatement(sql);
+             ps.setInt(1, id);
+             
+             if(ps.executeUpdate()!=0){
+             
+                 borrado=true;
+             }
+             ps.close();
+         } catch (SQLException ex) {
+             JOptionPane.showMessageDialog(null, "Error de sintaxis ");
+         }
+         
+         
+         return borrado;
+     }
+        
+        public boolean modificarMateria(Materia materia){
+     
+         String sql="UPDATE materia SET nombre = ?, anio = ?, activo = ? WHERE idMateria = ?";
+         boolean modificado=false;
+     try {
+             PreparedStatement ps= con.prepareStatement(sql);
+             ps.setString(1, materia.getNombre());
+             ps.setLong(2, materia.getAnio());
+             ps.setBoolean(3, materia.isActivo());
+             ps.setInt(4, materia.getIdMateria());
+             
+             if(ps.executeUpdate()!=0){
+             
+                 modificado=true;
+             }
+             ps.close();
+         } catch (SQLException ex) {
+             JOptionPane.showMessageDialog(null, "Error de sintaxis ");
+         }
+     return modificado;
+     }
+        
+        
+        
+        
+        
+        
     }
-
-    public boolean borrarAlumno(int id) {
-
-        boolean borrado = false;
-        String sql = "UPDATE alumno SET activo = 0 WHERE idAlumno = ?";
-        try {
-
-            PreparedStatement ps = con.prepareStatement(sql);
-            ps.setInt(1, id);
-
-            if (ps.executeUpdate() != 0) {
-
-                borrado = true;
-            }
-            ps.close();
-        } catch (SQLException ex) {
-            JOptionPane.showMessageDialog(null, "Error de sintaxis ");
-        }
-
-        return borrado;
-    }
-
-    public boolean modificarAlumno(Alumno alumno) {
-
-        String sql = "UPDATE alumno SET nombre = ?, apellido = ?, fechNac = ?, dni = ?, activo = ? WHERE idAlumno = ?";
-        boolean modificado = false;
-        try {
-            PreparedStatement ps = con.prepareStatement(sql);
-            ps.setString(1, alumno.getNombre());
-            ps.setString(2, alumno.getApellido());
-            ps.setDate(3, Date.valueOf(alumno.getFechNac()));
-            ps.setLong(4, alumno.getDni());
-            ps.setBoolean(5, alumno.isActivo());
-            ps.setInt(6, alumno.getIdAlumno());
-
-            if (ps.executeUpdate() != 0) {
-
-                modificado = true;
-            }
-            ps.close();
-        } catch (SQLException ex) {
-            JOptionPane.showMessageDialog(null, "Error de sintaxis ");
-        }
-        return modificado;
-    }
-}
